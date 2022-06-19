7508d2001ea6739982c15825dd6e1b393bdf84fc
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2022-06-19 15:35:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,13 @@ def main():
         x_new = 0.5 * coord[0] + 1
         y_new = 0.5 * coord[1]
         return x_new, y_new
+      
+    num_markers = st.number_input('number of points', min_value=100, max_value=500000, value = 100000) 
 
     # Generate coordinates
     x_values, y_values = [0], [0]
     x, y = 0, 0
-    for i in range(100000): #1000000
+    for i in range(num_markers):
         # Select transformation
         coord_change_method = choice([coord_change_method1,coord_change_method2,coord_change_method3])
         # Generate new coords
@@ -38,7 +40,7 @@ def main():
     colours = {'red':'r','blue':'b','green':'g','yellow':'y','black':'k'}
     markers = {'point':'.', 'circle':'o', 'star':'*', 'cross':'x'}
     
-
+    
     markersize = st.number_input('marker size', min_value=0.01, max_value=1.0, value=0.5) 
     colour = st.selectbox('colour', [x for x in colours.keys])
     marker = st.selectbox('shape', [x for x in markers.keys])'
